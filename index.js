e4230b8029c6cc8a18acb0b68cf8f2201451059c
Nameless Name <62650188+MarkMandriota@users.noreply.github.com>
2022-09-04 13:46:09 +0200

    fix

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const header = document.querySelector('h1')
-
 window.onload = () => {
-    let count = 0
-    const thinking = setInterval(() => {
-        header.innerHTML += '.'
-        if (count >= 3) clearInterval(thinking);
-    }, 500)
+
 }
