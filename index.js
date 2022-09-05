6e08affd60f38530ac97b766617a775c9b066616
Nameless Name <62650188+MarkMandriota@users.noreply.github.com>
2022-09-05 15:03:23 +0200

    flexing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const info_general = document.getElementById("info_general")
 
 window.onload = () => {
-    let years = Math.floor((Date.now()-new Date("2007-01-11"))/(86400000*365))
+    let years = Math.floor((Date.now()-new Date("2007-01-11"))/(86400000*365.25))
     info_general.innerText = info_general.innerText.replace("{{age}}", `${years} years old`)
 }
