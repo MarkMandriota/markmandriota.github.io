daba94d2dc00afac712bee6cb954e3230a9c5f19
Nameless Name <62650188+MarkMandriota@users.noreply.github.com>
2022-09-18 16:40:18 +0200

    refactor

--- index.js
+++
@@ -1,6 +0,0 @@
-const info_general = document.getElementById("info_general")
-
-window.onload = () => {
-    let years = Math.floor((Date.now()-new Date("2007-01-11"))/(86400000*365.25))
-    info_general.innerText = info_general.innerText.replace("{{age}}", `${years} years old`)
-}
